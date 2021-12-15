fcab62ca68965e6a07e52505e5f846501fbaf896
Felipe Sales <soufeliposales@gmail.com>
2021-12-14 23:10:46 -0300

    configuring SqlAdmConnection

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,9 +1,29 @@
 import 'package:flutter/material.dart';
 
+import 'core/database/sqlite_adm_connection.dart';
 import 'modules/splash/splash_page.dart';
 
-class AppWidget extends StatelessWidget {
+class AppWidget extends StatefulWidget {
   const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  State<AppWidget> createState() => _AppWidgetState();
+}
+
+class _AppWidgetState extends State<AppWidget> {
+  var sqliteAdmConnection = SqliteAdmConnection();
+
+  @override
+  initState() {
+    super.initState();
+    WidgetsBinding.instance?.addObserver(sqliteAdmConnection);
+  }
+
+  @override
+  dispose() {
+    WidgetsBinding.instance?.removeObserver(sqliteAdmConnection);
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
