06549761d1da2dcce60731aa06ff53ffb224eccb
Felipe Sales <soufeliposales@gmail.com>
2021-12-15 21:47:51 -0300

    configuring firebase_auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'app/app_widget.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(
     const AppWidget(),
   );
