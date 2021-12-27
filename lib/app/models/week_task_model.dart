6bafd7a8414d9d8ac7bd5ed836228cde7f17b4a3
Felipe Sales <soufeliposales@gmail.com>
2021-12-27 11:05:09 -0300

    search tasks (repository and service)

---
+++ lib/app/models/week_task_model.dart
@@ -0,0 +1,14 @@
+
+import 'task_model.dart';
+
+class WeekTaskModel {
+  final DateTime startDate;
+  final DateTime endDate;
+  final List<TaskModel> tasks;
+
+  WeekTaskModel({
+    required this.startDate,
+    required this.endDate,
+    required this.tasks,
+  });
+}
--- lib/app/repositories/tasks/tasks_repository_impl.dart
+++ lib/app/repositories/tasks/tasks_repository_impl.dart
@@ -1,4 +1,5 @@
 import 'package:todo_list/app/core/database/sqlite_connection_factory.dart';
+import 'package:todo_list/app/models/task_model.dart';
 import 'tasks_repository.dart';
 
 class TasksRepositoryImpl extends TasksRepository {
@@ -20,5 +21,38 @@ class TasksRepositoryImpl extends TasksRepository {
         'finish': 0,
       },
     );
+  }
+
+  @override
+  Future<List<TaskModel>> findByPeriod(DateTime start, DateTime end) async {
+    final startFilter = DateTime(
+      start.year,
+      start.month,
+      start.day,
+    );
+    final endFilter = DateTime(
+      start.year,
+      start.month,
+      start.day,
+    );
+
+    final connection = await _sqliteConnectionFactory.openConnection();
+    final result = await connection.rawQuery(
+      '''
+        select * 
+        from todo 
+        where implementing_parameters_filter_home between ? and ?
+        order by date_hour
+      ''',
+      [
+        startFilter.toIso8601String(),
+        endFilter.toIso8601String(),
+      ],
+    );
+    return result
+        .map(
+          (e) => TaskModel.loadFromDB(e),
+        )
+        .toList();
   }
 }
