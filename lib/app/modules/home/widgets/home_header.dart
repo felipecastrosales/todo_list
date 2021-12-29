cb39dbe2cad223bb4170d7ad1a30e65c3d9f3e71
Felipe Sales <soufeliposales@gmail.com>
2021-12-29 13:33:48 -0300

    resolving layout problem

--- lib/app/modules/home/widgets/home_header.dart
+++ lib/app/modules/home/widgets/home_header.dart
@@ -10,23 +10,19 @@ class HomeHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 20),
-          child: Selector<AuthProvider, String>(
-            selector: (context, authProvider) =>
-                authProvider.user?.displayName ?? 'Não informado',
-            builder: (_, value, __) {
-              return Text(
-                'E aí, ${value.trim()}!',
-                style: context.textTheme.headline5
-                    ?.copyWith(fontWeight: FontWeight.bold),
-              );
-            },
-          ),
-        ),
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20),
+      child: Selector<AuthProvider, String>(
+        selector: (context, authProvider) =>
+            authProvider.user?.displayName ?? 'Não informado',
+        builder: (_, value, __) {
+          return Text(
+            'E aí, ${value.trim()}!',
+            style: context.textTheme.headline5
+                ?.copyWith(fontWeight: FontWeight.bold),
+          );
+        },
+      ),
     );
   }
 }
