41d0d734c894a6885b713505da933f2169f5112f
Felipe Sales <soufeliposales@gmail.com>
2024-08-11 19:08:29 -0300

    style: enhanced padding of component

--- lib/app/modules/home/widgets/home_week.dart
+++ lib/app/modules/home/widgets/home_week.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:date_picker_timeline/date_picker_timeline.dart';
 import 'package:provider/provider.dart';
+import 'package:todo_list/app/core/ui/app_constants.dart';
 
 import 'package:todo_list/app/core/ui/theme_extensions.dart';
 import 'package:todo_list/app/models/task_filter_enum.dart';
@@ -20,32 +21,38 @@ class HomeWeek extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           const SizedBox(height: 20),
-          Text(
-            'DIA DA SEMANA',
-            style: context.titleStyle,
+          Padding(
+            padding: AppConstants.defaultComponentPadding,
+            child: Text(
+              'DIA DA SEMANA',
+              style: context.titleStyle,
+            ),
           ),
           const SizedBox(height: 10),
           SizedBox(
             height: 95,
-            child: Selector<HomeController, DateTime>(
-              selector: (context, controller) =>
-                  controller.initialDateOfWeek ?? DateTime.now(),
-              builder: (context, initialDateOfWeek, child) {
-                return DatePicker(
-                  initialDateOfWeek,
-                  height: 2,
-                  initialSelectedDate: initialDateOfWeek,
-                  selectionColor: context.primaryColor,
-                  selectedTextColor: Colors.white,
-                  daysCount: 7,
-                  monthTextStyle: const TextStyle(fontSize: 8),
-                  dayTextStyle: const TextStyle(fontSize: 13),
-                  dateTextStyle: const TextStyle(fontSize: 13),
-                  onDateChange: (date) {
-                    context.read<HomeController>().filterByDay(date);
-                  },
-                );
-              },
+            child: Padding(
+              padding: AppConstants.defaultComponentPadding,
+              child: Selector<HomeController, DateTime>(
+                selector: (context, controller) =>
+                    controller.initialDateOfWeek ?? DateTime.now(),
+                builder: (context, initialDateOfWeek, child) {
+                  return DatePicker(
+                    initialDateOfWeek,
+                    height: 2,
+                    initialSelectedDate: initialDateOfWeek,
+                    selectionColor: context.primaryColor,
+                    selectedTextColor: Colors.white,
+                    daysCount: 7,
+                    monthTextStyle: const TextStyle(fontSize: 8),
+                    dayTextStyle: const TextStyle(fontSize: 13),
+                    dateTextStyle: const TextStyle(fontSize: 13),
+                    onDateChange: (date) {
+                      context.read<HomeController>().filterByDay(date);
+                    },
+                  );
+                },
+              ),
             ),
           ),
         ],
